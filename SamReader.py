08dd5942d9101cdfa49e78745f7ced9be4aeaea8
Benoit <benoit-aliaga@orange.fr>
2020-11-12 22:40:39 +0100

    Ajout des fonctions

---
+++ SamReader.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python
+#-*- coding : utf-8 -*-
+
+"""
+SamReader
+
+SamReader is a parser program for samtool files (.sam). 
+
+How to launch SamReader?
+
+./SamReader.py argument 1 argument 2
+
+argument 1: samtool file (.sam)
+argument 2: output table
+ 
+"""
+
+__authors__ = ("Benoit Aliaga", "Matthieu Angles")
+__contact__ = ("aliaga.benoit@gmail.com", "matthieu.angles@hotmail.fr")
+__version__ = "0.0.1"
+__date__ = "11/11/2020"
+
+import sys, re
+
+def openSam():
+    """
+    Docstring
+    """
+    pass
+
+def parseFlag():
+    """
+    Docstring
+    """
+    pass
+
+def parseCigar():
+    """
+    Docstring
+    """
+    pass
+
+def main():
+    """
+    Docstring
+    """
+    pass
+
+if __name__ == "__main__":
+    main()
