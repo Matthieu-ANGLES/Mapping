b0d2f28daaa3031fa5bdaceb137b5d22f9cd263e
Mattiheu-ANGLES <matthieu.angles@hotmail.fr>
2020-11-15 19:04:42 +0100

    lecture cigar et listes valeurs/lettres ok, reste à ajouter la correspondancedes lettres

---
+++ cigar_output.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python
+# -*- coding : utf-8 -*-
+
+import sys, re
+
+cigar = input(str("Entrer une valeur de cigar :"))
+# exemple cigar 3M1I3M1D5M
+# autre exemple 312M41I38M1D669M
+
+ext = re.findall('\w',cigar)
+print (ext)
+
+valeur=[]
+lettre=[]
+val=""
+
+for i in range (0,len(ext)) :
+    if (ext[i]=='M' or ext[i]=='D' or ext[i]=='I' or ext[i]=='S' or ext[i]=='H' 
+                                    or ext[i]=="=" or ext[i]=='X') :
+        lettre.append(ext[i])
+        valeur.append(val)
+        val=""
+    else :
+        val=""+val+ext[i]
+
+#print (lettre)
+#print (valeur)
+
+for i in range (0, len(lettre)):
+    print (valeur[i],lettre[i])
+
