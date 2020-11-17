a9c9c11e3874026be54ad2897a1425684fcb1331
Mattiheu-ANGLES <matthieu.angles@hotmail.fr>
2020-11-17 09:11:54 +0100

    modif du 17/11/12

--- cigar_output.py
+++ cigar_output.py
@@ -7,7 +7,7 @@ cigar = input(str("Entrer une valeur de cigar :"))
 # exemple cigar 3M1I3M1D5M
 # autre exemple 312M41I38M1D669M
 
-ext = re.findall('\w',cigar)
+ext = re.findall('\w',cigar) # re fonction, donne une liste chaque éléments individualisés
 print (ext)
 
 valeur=[]
@@ -16,7 +16,7 @@ val=""
 
 for i in range (0,len(ext)) :
     if (ext[i]=='M' or ext[i]=='D' or ext[i]=='I' or ext[i]=='S' or ext[i]=='H' 
-                                    or ext[i]=="=" or ext[i]=='X') :
+                    or ext[i]=="=" or ext[i]=='X'or ext[i]=='N'or ext[i]=='P') :
         lettre.append(ext[i])
         valeur.append(val)
         val=""
