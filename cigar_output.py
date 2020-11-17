89bb0ff00333351e1427c8a931d3e00da4a008a1
Mattiheu-ANGLES <matthieu.angles@hotmail.fr>
2020-11-17 14:52:34 +0100

    bug du dictionnaire

--- cigar_output.py
+++ cigar_output.py
@@ -8,24 +8,43 @@ cigar = input(str("Entrer une valeur de cigar :"))
 # autre exemple 312M41I38M1D669M
 
 ext = re.findall('\w',cigar) # re fonction, donne une liste chaque éléments individualisés
-print (ext)
+#print (ext)
 
-valeur=[]
-lettre=[]
+value=[]
+key=[]
 val=""
 
 for i in range (0,len(ext)) :
     if (ext[i]=='M' or ext[i]=='D' or ext[i]=='I' or ext[i]=='S' or ext[i]=='H' 
                     or ext[i]=="=" or ext[i]=='X'or ext[i]=='N'or ext[i]=='P') :
-        lettre.append(ext[i])
-        valeur.append(val)
+        key.append(ext[i])
+        value.append(val)
         val=""
     else :
         val=""+val+ext[i]
 
-#print (lettre)
-#print (valeur)
+value = [int(i) for i in value]
 
-for i in range (0, len(lettre)):
-    print (valeur[i],lettre[i])
+print (key)     # = ['M',...]
+print (value)   # = [3, ...]
+
+#for i in range (0, len(key)):
+    #print (value[i],key[i])
+
+dico={k:v for k,v in zip(key,value)}
+
+print(len(key))
+dico2={}
+n=0
+for i in key:
+    if i not in dico2.keys():
+        dico2[key[i]]=value[n]
+    else:
+        dico2[key[i]]+=value[n]
+n=+1
+
+
+print (dico)
+print (dico2)
+
 
