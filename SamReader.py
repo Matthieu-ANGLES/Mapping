d161e79a31b27eeea7acbcf45203b6952766e8b1
Benoit <benoit-aliaga@orange.fr>
2020-11-21 13:46:30 +0100

    tableau flag percentage OK

--- SamReader.py
+++ SamReader.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 #-*- coding : utf-8 -*-
 
 """
@@ -18,10 +18,10 @@ argument 2: output table
 __authors__ = ("Benoit Aliaga", "Matthieu Angles")
 __contact__ = ("aliaga.benoit@gmail.com", "matthieu.angles@hotmail.fr")
 __version__ = "0.0.1"
-__date__ = "11/14/2020"
+__date__ = "11/21/2020"
 
 import os, sys, re, getopt
-from FlagDescription import FLAG
+#from FlagDescription import FLAG
 
 def usage():
     sys.stderr.write('''
@@ -77,7 +77,8 @@ def outFile(argv):
     """
     Output file name
     """
-    os.remove("table_flag.txt")
+    os.remove("parse_flag_table.txt")
+    os.remove("count_flag_table.txt")
     os.rename("Final_Flag_table.txt", argv)
     
 def parseFlag(sam_line):
@@ -85,7 +86,7 @@ def parseFlag(sam_line):
     Docstring
     """
     cpt = 1 # Initialisation d'un compteur
-    with open("table_flag.txt", "w") as output_sam_flag:
+    with open("parse_flag_table.txt", "w") as output_sam_flag:
         for line in sam_line:
             flag = line.split("\t") # Fractionnement de la line suivant \t
             if cpt == 1:
@@ -103,7 +104,7 @@ def countFlag():
     """
     Docstring
     """
-    with open("table_flag.txt", "r") as table_flag, open("Final_Flag_table.txt", "w") as output_flag:
+    with open("parse_flag_table.txt", "r") as table_flag, open("count_flag_table.txt", "w") as output_flag:
         dico = {}
         for line in table_flag:
             flag = line.rstrip("\n").rsplit(";")
@@ -116,6 +117,28 @@ def countFlag():
         for key in dico.keys():
             output_flag.write(key + ";" + str(dico[key]) + "\n")
 
+def total():
+    with open("count_flag_table.txt", "r") as output_sam_flag:
+        total = 0
+    
+        for line in output_sam_flag:
+            #print(line)
+            number = line.rstrip("\n").split(";")
+            numberi = int(number[1])
+            #print(numberi)
+            total = numberi + total
+        #print(total)
+        return total
+
+def percFlag(total):
+    percList = []
+    with open("count_flag_table.txt", "r") as output_sam_flag, open("Final_Flag_table.txt", "w") as FinalFlag:
+        for line in output_sam_flag:
+            number2 = line.rstrip("\n").split(";")
+            perc = (int(number2[1])*100) / total
+            newline = ";".join([number2[0],number2[1],str(round(perc,4))])
+            FinalFlag.write(number2[0] + ";" + number2[1] + ";" + str(round(perc,4)) + "\n")
+            
 def parseCigar():
     """
     Docstring
@@ -144,13 +167,25 @@ def main(argv):
             if current_argument in ("-h", "--help"):
                 usage()
             if current_argument in ("-i", "--input"):
-                print("Read the file")
+                print("Read the file.")
                 resSam = openSam(current_value)
+
+                print("Parse the flag.")
                 parseFlag(resSam)
+
+                print("Count the number of read for each flag combinations.")
                 countFlag()
+
+                print("Calculate the percentage for each flag combination.")
+                numberReads = total()
+                percFlag(numberReads)
+                
             if current_argument in ("-o", "--output"):
-                print("Ouput the file")
+                print("Ouput the file.")
                 outFile(current_value)
+
+        print("Analyse finished.")
+                
     except getopt.error as err:
         # Output error, and return with an error code
         print (str(err))
